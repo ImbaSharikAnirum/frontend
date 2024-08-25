9483f7834305e2b96ebd878a4dea228db0f6ab8b
igor Sharin <sharinigor1@gmail.com>
2024-08-25 15:08:10 +0900

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello World!</p>
-        <a className="App-link" href="https://react.dev/learn" target="_blank" rel="noreferrer noopener">Learn React</a>
+    
       </header>
     </div>
   );
