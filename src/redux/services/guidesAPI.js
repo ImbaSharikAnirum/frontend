98a5cf209337097094acc48159791481e3c64e7a
igor Sharin <sharinigor1@gmail.com>
2024-12-02 10:22:56 +0900

    fix

--- src/redux/services/guidesAPI.js
+++ src/redux/services/guidesAPI.js
@@ -1,25 +1,16 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const API_BASE_URL = "https://api.pinterest.com/v5"; // Правильный URL для Pinterest API
+const API = process.env.REACT_APP_API;
 
 export const guidesAPI = createApi({
   reducerPath: "guidesAPI",
   baseQuery: fetchBaseQuery({
-    baseUrl: API_BASE_URL,
-    prepareHeaders: (headers) => {
-      const token = process.env.REACT_APP_PINTEREST_ACCESS_TOKEN;
-      if (!token) {
-        console.error("Token is missing");
-      }
-      headers.set("Authorization", `Bearer ${token}`);
-      headers.set("Content-Type", "application/json");
-      return headers;
-    },
+    baseUrl: API,
   }),
   endpoints: (builder) => ({
     searchPins: builder.query({
       query: (searchQuery) => ({
-        url: `/search/pins?query=${encodeURIComponent(searchQuery)}`,
+        url: `/pinterest/search?query=${encodeURIComponent(searchQuery)}`,
         method: "GET",
       }),
     }),
