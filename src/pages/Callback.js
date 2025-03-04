86a7f0f91017e57e3787aab2dfe5acb42a11f0d9
igor Sharin <sharinigor1@gmail.com>
2025-03-04 17:48:20 +0900

    fix

--- src/pages/Callback.js
+++ src/pages/Callback.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { setPinterestToken } from "../redux/reducers/authReducer";
 
 const Callback = () => {
   const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const code = new URLSearchParams(window.location.search).get("code");
@@ -19,18 +21,32 @@ const Callback = () => {
             dispatch(setPinterestToken({ token: access_token })); // Сохраняем токен в Redux
             console.log(message); // Выводим сообщение из ответа сервера
           } else {
+            setError("Токен не получен");
             console.error("Токен не получен");
-            // Можно показать ошибку на UI
           }
         })
         .catch((error) => {
+          setError("Ошибка при получении токена");
           console.error("Ошибка при получении токена: ", error);
-          // Можете обработать ошибку на UI
+        })
+        .finally(() => {
+          setLoading(false); // Завершаем загрузку
         });
+    } else {
+      setError("Отсутствует код авторизации");
+      setLoading(false); // Завершаем загрузку, если код отсутствует
     }
   }, [dispatch]);
 
-  return <div>Loading...</div>;
+  if (loading) {
+    return <div>Загрузка...</div>; // Состояние загрузки
+  }
+
+  if (error) {
+    return <div>Ошибка: {error}</div>; // Ошибка
+  }
+
+  return <div>Успешно авторизовались через Pinterest!</div>;
 };
 
 export default Callback;
